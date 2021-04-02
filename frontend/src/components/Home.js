68c49cd4f14cbd0aaa8bec0f2e116d68269bd16f
CodrKai <codrkai@gmail.com>
2021-04-01 18:27:51 -0700

    updated with mysql

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <section>
             <div class="container-fluid">
                 <h1 class="mt-5">Welcome</h1>
-                <p>This site was created using Node JS with React and MongoDB.</p>
+                <p>This site was created using Node JS with React and MySQL.</p>
             </div>
         </section>
     );
